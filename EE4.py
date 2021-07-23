073dd03ae091b8ea4314201af77e7838e80e7621
Jinu Park <85676560+jinubook@users.noreply.github.com>
2021-07-23 10:04:16 +0900

    Add EE4.PY

---
+++ EE4.py
@@ -0,0 +1,33 @@
+# Python Bash EE4 make C++ Making
+import sys;
+try:
+  u = open("EE4.make", "r")
+  arr = u.readlines();
+  sr = ""
+  for ln in arr:
+    sr += ln
+  msys = sr.split(';')
+  b = False
+  try:
+    # Linux Syntax, If You're Using Windows, Change It To Your Installation Directory
+    d = open("/usr/local/bin/gcc", "r");
+  except:
+    b = True
+  if b:
+    print("GCC Path Does Not Exist. Change It If You're Using Windows.")
+  else:
+    d = [""]
+    f = 0
+    for s in msys:
+      # Python 3.9 Syntax, If You're Using Older Versions, Upgrade It.
+      f = s.removesuffix("MAKE C: ");
+      d[f] = f
+      f += 1
+    print("Making C Code To ee4_gen.....")
+    s = ""
+    for a in d:
+      s += " " + a
+    os.system("gcc " + s)
+    print("Made ee4_gen")
+except:
+  print("EE4.make Does Not Exists.")
